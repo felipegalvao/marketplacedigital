e925bddbdea6bd881fef0517c884abf6a66f9a51
Felipe Galvao <contato@felipegalvao.com.br>
2016-11-07 15:50:08 -0200

    Fix por Pagseguro notification v2

--- marketplacedigital/shop/views.py
+++ marketplacedigital/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
